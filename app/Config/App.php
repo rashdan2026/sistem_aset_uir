13ad36c773982becf9931483a1821e9e236cd1fd
rashdan2026 <github@putrariau.com>
2026-05-05 06:19:57 +0700

    production

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/asetUIR/public/';
+    public string $baseURL = getenv('BASEURL') ?: 'http://localhost/asetUIR/public/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
