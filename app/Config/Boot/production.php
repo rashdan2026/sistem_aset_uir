45c79caf3bdc381fc8e08cf75a192dc59ab6f081
rashdan2026 <github@putrariau.com>
2026-05-05 07:27:37 +0700

    fix: remove config() call from production.php, fix env overrides, restore display_errors off

--- app/Config/Boot/production.php
+++ app/Config/Boot/production.php
@@ -9,8 +9,10 @@
  |
  | If you set 'display_errors' to '1', CI4's detailed error report will show.
  */
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
+error_reporting(E_ALL & ~E_DEPRECATED);
+// If you want to suppress more types of errors.
+// error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
+ini_set('display_errors', '0');
 
 /*
  |--------------------------------------------------------------------------
@@ -21,8 +23,3 @@ ini_set('display_errors', '1');
  | release of the framework.
  */
 defined('CI_DEBUG') || define('CI_DEBUG', false);
-
-if (getenv('BASEURL') !== false && getenv('BASEURL') !== '') {
-    $appConfig = config(App::class);
-    $appConfig->baseURL = getenv('BASEURL');
-}
